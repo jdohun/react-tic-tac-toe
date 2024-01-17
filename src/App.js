2e251010c339ef2bd290bcb93a3c5edea702057c
jdohun <wjdehgns97@naver.com>
2024-01-17 14:53:24 +0900

    feat: 다음 플레이어의 표시 마크를 출력하고 게임 종료시 승리자를 출력하는 기능 추가

--- src/App.js
+++ src/App.js
@@ -20,8 +20,17 @@ const Board = () => {
         setXisNext(!xIsNext);   // xIsNext 업데이트
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
                 <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
